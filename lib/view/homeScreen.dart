bf175aaa4d7d40c83e97db1b38fa7e582ad169fa
sreedharshm <sreedharshm@gmail.com>
2023-04-04 22:04:47 +0530

    debugfix

--- lib/view/homeScreen.dart
+++ lib/view/homeScreen.dart
@@ -78,7 +78,7 @@ class _HomeScreenState extends State<HomeScreen> {
           BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
           BottomNavigationBarItem(icon: Icon(Icons.add), label: "Add"),
           BottomNavigationBarItem(
-              icon: Icon(Icons.notifications), label: "Notification"),
+              icon: Icon(Icons.notifications), label: "Notifications"),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
         ],
       ),
