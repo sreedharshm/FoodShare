8e244546cb8bdd4d1e2eaf24b472aab9d8ccf527
saurav152002 <sauravs152002@gamil.com>
2023-04-04 22:37:36 +0530

    aasasas

--- lib/view/homeScreen.dart
+++ lib/view/homeScreen.dart
@@ -73,11 +73,11 @@ class _HomeScreenState extends State<HomeScreen> {
         onTap: onTap,
         currentIndex: value,
         selectedItemColor: primary,
-        unselectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-           BottomNavigationBarItem(icon: Icon(Icons.add), label: "Add"),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: "Add"),
           BottomNavigationBarItem(
               icon: Icon(Icons.notifications), label: "Notifications"),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
