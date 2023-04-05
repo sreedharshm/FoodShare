a61cb3976aa99a99d66a124d94343fe546bd7094
saurav152002 <sauravs152002@gamil.com>
2023-04-05 12:52:37 +0530

    hellohello

--- lib/view/add_screen.dart
+++ lib/view/add_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:foodproject/main.dart';
 
 class Add extends StatelessWidget {
   const Add({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("container"),);
+    Scaffold(
+      appBar: AppBar(
+        title: const Text("Donate now"),
+      ),
+    );
   }
 }
