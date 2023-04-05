c1ccc02f6d0895e37fe62c8c92fc7cf4ff8cf517
saurav152002 <sauravs152002@gamil.com>
2023-04-05 13:27:16 +0530

    some google fonts

--- lib/view/add_screen.dart
+++ lib/view/add_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:foodproject/main.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Add extends StatelessWidget {
   const Add({super.key});
@@ -10,7 +11,13 @@ class Add extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(),
+        shadowColor: Colors.transparent,
+        backgroundColor: const Color.fromARGB(255, 241, 241, 241),
+        centerTitle: true,
+        titleTextStyle: GoogleFonts.montserrat(
+            color: const Color.fromARGB(255, 130, 130, 130),
+            fontSize: 20,
+            fontWeight: FontWeight.normal),
         title: const Text("Donate now"),
       ),
     );
