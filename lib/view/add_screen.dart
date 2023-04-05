0cab821e0d644daceeccd160f950630db72bee4b
saurav152002 <sauravs152002@gamil.com>
2023-04-05 20:55:48 +0530

    add_screen_stepper

--- lib/view/add_screen.dart
+++ lib/view/add_screen.dart
@@ -5,22 +5,31 @@ import 'package:foodproject/main.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'color.dart';
 
+List<Step> getSteps() => [
+      Step(title: Text('Food item'), content: Container()),
+      Step(title: Text("Address"), content: Container()),
+      Step(title: Text("Time"), content: Container())
+    ];
+
 class Add extends StatelessWidget {
   const Add({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        shadowColor: Colors.transparent,
-        backgroundColor: tdGrey,
-        centerTitle: true,
-        titleTextStyle: GoogleFonts.montserrat(
-            color: const Color.fromARGB(255, 130, 130, 130),
-            fontSize: 20,
-            fontWeight: FontWeight.normal),
-        title: const Text("Donate now"),
-      ),
-    );
+        appBar: AppBar(
+          shadowColor: Colors.transparent,
+          backgroundColor: tdGrey,
+          centerTitle: true,
+          titleTextStyle: GoogleFonts.montserrat(
+              color: const Color.fromARGB(255, 130, 130, 130),
+              fontSize: 20,
+              fontWeight: FontWeight.normal),
+          title: const Text("Donate now"),
+        ),
+        body: Stepper(
+          type: StepperType.horizontal,
+          steps: getSteps(),
+        ));
   }
 }
