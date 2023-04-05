acd144f658b405dc4c3fcbc3ce7fb3f8bce7431f
sreedharshm <sreedharshm@gmail.com>
2023-04-05 10:09:57 +0530

    ss

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
 class HomeScreen extends StatelessWidget {
