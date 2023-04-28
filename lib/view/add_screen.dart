b4fc55bf574285ca69f84c7def14dd9f649b4274
saurav152002 <sauravs152002@gamil.com>
2023-04-28 17:07:02 +0530

    addscreen, profile, about

--- lib/view/add_screen.dart
+++ lib/view/add_screen.dart
@@ -110,7 +110,6 @@ class _MyAddPageState extends State<MyAddPage> {
               colorScheme: const ColorScheme.light(primary: tdBlue),
             ),
             child: Stepper(
-              physics: ScrollPhysics(parent:),
               type: StepperType.horizontal,
               steps: getSteps(),
               currentStep: currentStep,
