281004d6a233ae17f8becfca90dbb40c42a5a395
saurav152002 <sauravs152002@gamil.com>
2023-06-02 16:58:48 +0530

    search

--- lib/view/search_screen.dart
+++ lib/view/search_screen.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:foodproject/view/color.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-String name = " ";
+String name = "";
 
 class Search extends StatefulWidget {
   const Search({super.key});
@@ -17,41 +17,26 @@ class Search extends StatefulWidget {
 class _SearchState extends State<Search> {
   final CollectionReference orgDetails =
       FirebaseFirestore.instance.collection('Organisations');
-  // final user = FirebaseAuth.instance.currentUser;
-  // bool isLoading = false;
 
-  // List<Map<String, dynamic>> _allUsers = [];
-
-  // //list docIDs
-  // List<String> docIDs = [];
-
-  //get docIDs
-  // Future<void> getDocId() async {
-  //   print('start');
-  //   await FirebaseFirestore.instance.collection('Organisations').get().then(
-  //         (snapshot) => snapshot.docs.forEach(
-  //           (document) {
-  //             print(document.reference);
-  //             docIDs.add(document.reference.id);
-  //           },
-  //         ),
-  //       );
-  // }
-  // Example: Print debug statements
+  final TextEditingController _searchName = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          toolbarHeight: 75,
           shadowColor: Colors.transparent,
           backgroundColor: tdBGcolor,
-          toolbarHeight: 60,
-          centerTitle: true,
-          title: const Text('Search'),
-          titleTextStyle: GoogleFonts.montserrat(
-              color: const Color.fromARGB(255, 130, 130, 130),
-              fontSize: 20,
-              fontWeight: FontWeight.normal),
+          title: Card(
+            child: TextField(
+              controller: _searchName,
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search), hintText: "search here"),
+              onChanged: (value) => setState(() {
+                name = value;
+              }),
+            ),
+          ),
         ),
         body: StreamBuilder(
             stream: orgDetails.orderBy('name').snapshots(),
@@ -60,59 +45,143 @@ class _SearchState extends State<Search> {
                 return ListView.builder(
                   itemCount: snapshot.data!.docs.length,
                   itemBuilder: (context, index) {
-                    final DocumentSnapshot orgSnap = snapshot.data!.docs[index];
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 255, 255, 255),
-                            borderRadius: BorderRadius.circular(25),
-                            boxShadow: [
-                              BoxShadow(
-                                  color:
-                                      const Color.fromARGB(255, 200, 200, 200)
-                                          .withOpacity(0.1),
-                                  spreadRadius: 5,
-                                  blurRadius: 5)
-                            ]),
-                        height: 70,
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(5),
-                              child: CircleAvatar(
-                                radius: 30,
-                                backgroundImage: NetworkImage(
-                                    orgSnap['acc_image'].toString()),
-                              ),
-                            ),
-                            Flexible(
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Flexible(
-                                      child: Text(
-                                        orgSnap['name'],
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                    ),
-                                    Flexible(
-                                        child: Text(
-                                      orgSnap['address'],
-                                      style: TextStyle(fontSize: 15),
-                                    ))
-                                  ],
+                    var orgSnap = snapshot.data!.docs[index].data()
+                        as Map<String, dynamic>;
+
+                    if (name.isEmpty) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              borderRadius: BorderRadius.circular(25),
+                              boxShadow: [
+                                BoxShadow(
+                                    color:
+                                        const Color.fromARGB(255, 200, 200, 200)
+                                            .withOpacity(0.1),
+                                    spreadRadius: 5,
+                                    blurRadius: 5)
+                              ]),
+                          height: 70,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(5),
+                                  child: CircleAvatar(
+                                    radius: 30,
+                                    backgroundImage: NetworkImage(
+                                        orgSnap['acc_image'].toString()),
+                                  ),
                                 ),
-                              ),
-                            )
-                          ],
+                                Flexible(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Flexible(
+                                          child: Text(
+                                            orgSnap['name'],
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                        Flexible(
+                                            child: Text(
+                                          orgSnap['address'],
+                                          style: TextStyle(fontSize: 15),
+                                        ))
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                IconButton(
+                                    icon: const Icon(Icons.arrow_circle_right),
+                                    iconSize: 40,
+                                    color: tdBlue,
+                                    splashColor: Colors.grey,
+                                    onPressed: () {})
+                              ],
+                            ),
+                          ),
                         ),
-                      ),
-                    );
+                      );
+                    }
+                    if (orgSnap['name']
+                        .toString()
+                        .toLowerCase()
+                        .startsWith(name.toLowerCase())) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              borderRadius: BorderRadius.circular(25),
+                              boxShadow: [
+                                BoxShadow(
+                                    color:
+                                        const Color.fromARGB(255, 200, 200, 200)
+                                            .withOpacity(0.1),
+                                    spreadRadius: 5,
+                                    blurRadius: 5)
+                              ]),
+                          height: 70,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(5),
+                                  child: CircleAvatar(
+                                    radius: 30,
+                                    backgroundImage: NetworkImage(
+                                        orgSnap['acc_image'].toString()),
+                                  ),
+                                ),
+                                Flexible(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Flexible(
+                                          child: Text(
+                                            orgSnap['name'],
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                        Flexible(
+                                            child: Text(
+                                          orgSnap['address'],
+                                          style: TextStyle(fontSize: 15),
+                                        ))
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                IconButton(
+                                    icon: const Icon(Icons.arrow_circle_right),
+                                    iconSize: 40,
+                                    color: tdBlue,
+                                    splashColor: Colors.grey,
+                                    onPressed: () {})
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                    return Container();
                   },
                 );
               } else {
